634dd67608257a441c822c786832d6faa89fd850
david <nitin.porwal90@gmail.com>
2026-08-22 01:16:10 +0530

    feat : Add dependency injection example

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status, Depends
 from pydantic import BaseModel
 from typing import Optional
  
@@ -14,12 +14,25 @@ class User(BaseModel):
     email:str
     age: int
     phoneNo: Optional[str] = None
+    password : str
 
-@app.post("/user")
+class UserResponse(BaseModel):
+    id: int
+    name: str
+    email: str
+    age: int
+
+
+@app.post("/user", response_model = UserResponse, status_code = status.HTTP_201_CREATED)
 def create_user(user: User):
     return {
-        "message": "User Create Successfully REE",
-        "user": user
+        "id": 1,
+        "name": user.name,
+        "emami": user.email,
+        "age": user.age,
+        "password": "secret123"
+
+
     }   
 
 @app.get("/")
@@ -34,11 +47,6 @@ def hello():
         "message": "hello not radhe radhe"
     }
 
-@app.get("/users/{user_id}")
-def get_user(user_id : int):
-    return {
-        "user_id": user_id
-    }
 
 @app.get("/search")
 def search_user(name : str):
@@ -52,3 +60,48 @@ def create_users():
         "messge":  "User created successfully"
     }   
 
+@app.get("/users/{user_id}", response_model = UserResponse)
+def get_user(user_id:int):
+    if user_id != 1:
+         raise HTTPException(
+             status_code = 404,
+             detail = "User Not Found"
+         )
+
+    return {
+        "id":1,
+        "name": "nit",
+        "email": "mm@gmail.com",
+        "age": 23
+    }
+
+
+def get_current_user():
+    return {
+        "id": 1,
+        "name": "Ram",
+        "role": "Admin"
+    }
+
+@app.get("/profile")
+def profile(user = Depends(get_current_user)):
+    return {
+        "message": "Admin Data Fetch",
+        "user": user
+       
+    }
+
+def get_role_user():
+    role = "Admin"
+    return role
+
+@app.get("/roles")
+def get_role(role = Depends(get_role_user)):
+    if role != "Admin":
+        raise HTTPException(
+            status_code = 403,
+            detail = "ADMIN Access Denied"
+        )
+    return {
+        "message": "Welcome Admin"
+    }
