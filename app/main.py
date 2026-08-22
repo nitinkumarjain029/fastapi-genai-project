3457518b114ec6418d208eb2113cbc25f28f7306
david <nitin.porwal90@gmail.com>
2026-08-22 23:42:05 +0530

    refactor: separate user schemas

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, HTTPException, status, Depends
-from pydantic import BaseModel
-from typing import Optional
+from app.routers.users import router as users_router
  
 
 app = FastAPI(
@@ -9,31 +8,8 @@ app = FastAPI(
     version = "1.0.0"
 )
 
-class User(BaseModel):
-    name: str
-    email:str
-    age: int
-    phoneNo: Optional[str] = None
-    password : str
 
-class UserResponse(BaseModel):
-    id: int
-    name: str
-    email: str
-    age: int
-
-
-@app.post("/user", response_model = UserResponse, status_code = status.HTTP_201_CREATED)
-def create_user(user: User):
-    return {
-        "id": 1,
-        "name": user.name,
-        "emami": user.email,
-        "age": user.age,
-        "password": "secret123"
-
-
-    }   
+app.include_router(users_router)
 
 @app.get("/")
 def home():
@@ -48,55 +24,12 @@ def hello():
     }
 
 
-@app.get("/search")
-def search_user(name : str):
-    return {
-        "seraching for": name
-    }
-
-@app.post("/users")
-def create_users():
-    return {
-        "messge":  "User created successfully"
-    }   
-
-@app.get("/users/{user_id}", response_model = UserResponse)
-def get_user(user_id:int):
-    if user_id != 1:
-         raise HTTPException(
-             status_code = 404,
-             detail = "User Not Found"
-         )
-
-    return {
-        "id":1,
-        "name": "nit",
-        "email": "mm@gmail.com",
-        "age": 23
-    }
-
-
-def get_current_user():
-    return {
-        "id": 1,
-        "name": "Ram",
-        "role": "Admin"
-    }
-
-@app.get("/profile")
-def profile(user = Depends(get_current_user)):
-    return {
-        "message": "Admin Data Fetch",
-        "user": user
-       
-    }
-
 def get_role_user():
     role = "Admin"
     return role
 
 @app.get("/roles")
-def get_role(role = Depends(get_role_user)):
+def get_role(role = Depends(get_role_user)): 
     if role != "Admin":
         raise HTTPException(
             status_code = 403,
